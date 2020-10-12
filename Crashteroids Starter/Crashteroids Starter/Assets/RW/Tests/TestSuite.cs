002ce04eb1b41994240f6c5147219f41cdf3dfc3
C00205926 <c00205926@itcarlow.ie>
2020-10-12 11:23:05 +0100

    Driver Two Done

--- Crashteroids Starter/Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
+++ Crashteroids Starter/Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
@@ -7,24 +7,62 @@ public class TestSuite
 {
     private Game game;
 
+    [SetUp]
+    public void Setup()
+    {
+        GameObject gameGameObject = 
+            MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+        game = gameGameObject.GetComponent<Game>();
+    }
+
     // 1
     [UnityTest]
     public IEnumerator AsteroidsMoveDown()
     {
-        // 2
-        GameObject gameGameObject =
-            MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
-        game = gameGameObject.GetComponent<Game>();
-        // 3
         GameObject asteroid = game.GetSpawner().SpawnAsteroid();
-        // 4
         float initialYPos = asteroid.transform.position.y;
-        // 5
         yield return new WaitForSeconds(0.1f);
-        // 6
+  
         Assert.Less(asteroid.transform.position.y, initialYPos);
-        // 7
+    }
+
+    [UnityTest]
+    public IEnumerator GameOverOccursOnAsteroidCollision()
+    {
+        GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+        asteroid.transform.position = game.GetShip().transform.position;
+        yield return new WaitForSeconds(0.1f);
+
+        Assert.True(game.isGameOver);
+    }
+    [UnityTest]
+    public IEnumerator NewGameRestartsGame()
+    {
+        //1
+        game.isGameOver = true;
+        game.NewGame();
+        //2
+        Assert.False(game.isGameOver);
+        yield return null;
+    }
+    [UnityTest]
+    public IEnumerator LaserMovesUp()
+    {
+        // 1
+        GameObject laser = game.GetShip().SpawnLaser();
+        // 2
+        float initialYPos = laser.transform.position.y;
+        yield return new WaitForSeconds(0.1f);
+        // 3
+        Assert.Greater(laser.transform.position.y, initialYPos);
+    }
+
+
+    [TearDown]
+    public void Teardown()
+    {
         Object.Destroy(game.gameObject);
     }
+
 
 }
